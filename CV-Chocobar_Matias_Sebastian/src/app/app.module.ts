a30a40cace0947bb5531321bb46ab9f3d2e30295
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-09-08 05:50:20 -0300

    Porfin funciono el Background color

--- CV-Chocobar_Matias_Sebastian/src/app/app.module.ts
+++ CV-Chocobar_Matias_Sebastian/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
